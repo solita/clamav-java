fc05cdd9fc5eef3e4d60ed3199826348b63da2dd
Henrik Alstad <henrikalstad@gmail.com>
2016-02-24 23:28:23 +0100

    Changed identation of a few lines to be more consistent and similar to official oracle style

--- src/main/java/fi/solita/clamav/ClamAVClient.java
+++ src/main/java/fi/solita/clamav/ClamAVClient.java
@@ -19,34 +19,34 @@ public class ClamAVClient {
   private String hostName;
   private int port;
   private int timeout;
-  
+
   // "do not exceed StreamMaxLength as defined in clamd.conf, otherwise clamd will reply with INSTREAM size limit exceeded and close the connection."
   private static final int CHUNK_SIZE = 2048;
   private static final int DEFAULT_TIMEOUT = 500;
-  
+
   /**
    * @param timeout zero means infinite timeout. Not a good idea, but will be accepted.
    */
   public ClamAVClient(String hostName, int port, int timeout)  {
-    if (timeout < 0) throw new IllegalArgumentException("Negative timeout value does not make sense.");
+    if (timeout < 0) {
+      throw new IllegalArgumentException("Negative timeout value does not make sense.");
+    }
     this.hostName = hostName;
     this.port = port;
     this.timeout = timeout;
   }
- 
+
   public ClamAVClient(String hostName, int port) {
     this(hostName, port, DEFAULT_TIMEOUT);
   }
-   
+
   /**
    * Run PING command to clamd to test it is responding.
    * 
    * @return true if the server responded with proper ping reply.
    */
   public boolean ping() throws IOException {
-    try (Socket s = new Socket(hostName,port);  
-        OutputStream outs = s.getOutputStream(); )
-    {
+    try (Socket s = new Socket(hostName,port); OutputStream outs = s.getOutputStream()) {
       s.setSoTimeout(timeout);
       outs.write(asBytes("zPING\0"));
       outs.flush();
@@ -55,7 +55,7 @@ public class ClamAVClient {
       return Arrays.equals(b, asBytes("PONG"));
     }
   }
-  
+
   /**
    * Streams the given data to the server in chunks. The whole data is not kept in memory.
    * <p>
@@ -65,16 +65,14 @@ public class ClamAVClient {
    * @return server reply
    */
   public byte[] scan(InputStream is) throws IOException {
-    try (Socket s = new Socket(hostName,port);  
-        OutputStream outs = new BufferedOutputStream(s.getOutputStream()); )
-    {
+    try (Socket s = new Socket(hostName,port); OutputStream outs = new BufferedOutputStream(s.getOutputStream())) {
       s.setSoTimeout(timeout); 
-      
+
       // handshake
       outs.write(asBytes("zINSTREAM\0"));
       outs.flush();
       byte[] chunk = new byte[CHUNK_SIZE];
-      
+
       // send data
       int read = is.read(chunk);
       while (read >= 0) {
@@ -85,18 +83,18 @@ public class ClamAVClient {
         outs.write(chunk, 0, read);
         read = is.read(chunk);
       }
-      
+
       // terminate scan
       outs.write(new byte[]{0,0,0,0});
       outs.flush();
-      
+
       // read reply
-      try (InputStream clamIs = s.getInputStream();) {
+      try (InputStream clamIs = s.getInputStream()) {
         return readAll(clamIs);
       }
     } 
   }
-  
+
   /**
    * @param in data to scan
    * @return server reply
@@ -105,7 +103,7 @@ public class ClamAVClient {
     ByteArrayInputStream bis = new ByteArrayInputStream(in);
     return scan(bis);
   }
-  
+
   /**
    * @return true if no virus was found according to the clamd reply message
    */
@@ -113,7 +111,7 @@ public class ClamAVClient {
     String r = new String(reply, "ASCII");
     return (r.contains("OK") && !r.contains("FOUND"));
   }
-  
+
   // byte conversion based on ASCII character set regardless of the current system locale
   private static byte[] asBytes(String s) throws UnsupportedEncodingException {
     return s.getBytes("ASCII");
@@ -122,7 +120,7 @@ public class ClamAVClient {
   // reads all available bytes from the stream
   private static byte[] readAll(InputStream is) throws IOException {
     ByteArrayOutputStream tmp = new ByteArrayOutputStream();
-    
+
     byte[] buf = new byte[2000];
     int read = is.read(buf);
     while (read > 0) {
