1060a1ab1fe69f8f53a1622dac0349d54691eec8
Eugene.Roldukhin <Eugene.Roldukhin@nordigy.ru>
2019-03-20 19:10:36 +0300

    Added support for simple execution MULTISCAN command (only one file)

--- src/test/java/fi/solita/clamav/TestConfiguration.java
+++ src/test/java/fi/solita/clamav/TestConfiguration.java
@@ -1,6 +1,6 @@
 package fi.solita.clamav;
 
 public class TestConfiguration {
-    public static String CLAMAV_HOST = "192.168.99.100";
+    public static String CLAMAV_HOST = "localhost";
     public static int CLAMAV_PORT = 3310;
 }
